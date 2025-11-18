7ab8194f4fe77ebbb4b71337d9a63bbd13872252
samudaBR <samuda@republic.boutique>
2025-11-18 19:51:36 +0530

    mlruns updates

---
+++ train_final_model.py
@@ -0,0 +1,5 @@
+"""This script's only job is to train and 
+save the best model. separates my day-to-day 
+experimentation (run_pipeline.py) from the process of 
+creating a "blessed" production artifact."""
+
